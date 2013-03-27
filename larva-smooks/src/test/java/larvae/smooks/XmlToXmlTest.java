748c54766a190932b610612ef26069b49c5f2725
Tadayoshi Sato <tadayosi@gmail.com>
2013-03-27 22:31:16 +0900

    larva-smooks: introduced XMLUnit

--- larva-smooks/src/test/java/larvae/smooks/XmlToXmlTest.java
+++ larva-smooks/src/test/java/larvae/smooks/XmlToXmlTest.java
@@ -1,39 +1,40 @@
 package larvae.smooks;
 
-import static org.junit.Assert.*;
+import static org.custommonkey.xmlunit.XMLAssert.*;
 
+import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.Before;
 import org.junit.Test;
 
 public class XmlToXmlTest {
 
-  private static final String INPUT      = "input-xml-to-xml.xml";
+  private static final String INPUT    = "input-xml-to-xml.xml";
+  private static final String EXPECTED = "<aaa><bbb><ccc value=\"XXXXX\"/></bbb></aaa>";
 
-  private static final String EXPECTED_1 = "<aaa><bbb><ccc value=\"XXXXX\"></ccc></bbb></aaa>";
-  private static final String EXPECTED_2 = "<aaa><bbb><ccc value=\"XXXXX\" /></bbb></aaa>";
+  @Before
+  public void setUp() {
+    XMLUnit.setIgnoreWhitespace(true);
+  }
 
   @Test
   public void transform_XSLT() throws Exception {
     String result = new XmlToXml().transformXslt(INPUT);
     System.out.println(result);
-    assertXmlEquals(EXPECTED_1, result);
+    assertXMLEqual(EXPECTED, result);
   }
 
   @Test
   public void transform_FreeMarker() throws Exception {
     String result = new XmlToXml().transformFreeMarker(INPUT);
     System.out.println(result);
-    assertXmlEquals(EXPECTED_2, result);
+    assertXMLEqual(EXPECTED, result);
   }
 
   @Test
   public void transform_Visitor() throws Exception {
     String result = new XmlToXml().transformVisitor(INPUT);
     System.out.println(result);
-    assertXmlEquals(EXPECTED_1, result);
-  }
-
-  private static void assertXmlEquals(String expected, String result) {
-    assertEquals(expected, result.replaceAll(">\\s+<", "><"));
+    assertXMLEqual(EXPECTED, result);
   }
 
 }
