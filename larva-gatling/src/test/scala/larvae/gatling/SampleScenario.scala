a729868540c560c9f31571111ea5ba5017328360
Tadayoshi Sato <tadayosi@gmail.com>
2013-11-24 21:51:32 +0900

    gatling: test scenario refined; logging conf added

--- larva-gatling/src/test/scala/larvae/gatling/SampleScenario.scala
+++ larva-gatling/src/test/scala/larvae/gatling/SampleScenario.scala
@@ -1,5 +1,4 @@
 package larvae.gatling
-
 import scala.concurrent.duration._
 
 import io.gatling.core.Predef._
@@ -13,6 +12,10 @@ class SampleScenario extends Simulation {
     val scn = scenario("Sample Scenario")
         .exec(http("localhost").get("http://localhost:8080/larva-gatling"))
 
-    setUp(scn.inject(atOnce(10 users)));
+    setUp(scn.inject(
+        atOnce(3 user),
+        ramp(3 users) over (3 seconds),
+        constantRate(1 usersPerSec) during (3 seconds)))
+        .protocols(http.shareConnections);
 
 }
