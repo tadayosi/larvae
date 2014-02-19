4a4130cb9619cf68b3cfd32acab68a9e9e17c851
Tadayoshi Sato <tadayosi@gmail.com>
2014-02-19 10:36:43 +0900

    gatling: slightly improved scenario

--- larva-gatling/src/test/scala/larvae/gatling/SampleScenario.scala
+++ larva-gatling/src/test/scala/larvae/gatling/SampleScenario.scala
@@ -10,12 +10,22 @@ import io.gatling.http.Predef._
 class SampleScenario extends Simulation {
 
     val scn = scenario("Sample Scenario")
-        .exec(http("localhost").get("http://localhost:8080/larva-gatling"))
+        .exec(http("Get 1")
+            .get("http://localhost:8080/larva-gatling")
+            .check(
+                status.is(200),
+                bodyString.is("Hello")))
+        .pause(3)
+        .exec(http("Get 2")
+            .get("http://localhost:8080/larva-gatling")
+            .check(
+                status.not(404), status.not(500),
+                bodyString.is("Hello")))
 
     setUp(scn.inject(
         atOnce(3 user),
         ramp(3 users) over (3 seconds),
         constantRate(1 usersPerSec) during (3 seconds)))
-        .protocols(http.shareConnections);
+        .protocols(http.shareConnections)
 
 }
